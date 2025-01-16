7a8934789814c12dab2ccfeb2cc72d5162d11890
Toddy <oluwatoddyemma0406@gmail.com>
2025-01-16 22:58:26 +0100

    Initial commit with project files

---
+++ main.js
@@ -0,0 +1,27 @@
+// const menuBtn = document.getElementById("menu-btn");
+// const navLinks = document.getElementById("nav-links");
+// const menuBtnIcon = menuBtn.querySelector("i");
+
+// menuBtn.addEventListener("click", (e) => {
+//     navLinks.classList.toggle("open");
+
+//     const isOpen = navLinks.classList.contains("open");
+//     menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+// });
+
+// navLinks.addEventListener("click", (e) => {
+//     navLinks.classList.remove("open");
+//     menuBtnIcon.setAttribute("class", "ri-menu-line");
+// })
+
+// // function updateLogo() {
+// //     const logo = document.getElementById('logo');
+// //     if (window.innerWidth <= 768) {
+// //         logo.src = 'celar logo2.png';
+// //     } else {
+// //         logo.src = 'celar logo.png'
+// //     }
+// // }
+
+// // window.addEventListener('resize', updateLogo);
+// // document.addEventListener('DOMContentLoaded', updateLogo);
